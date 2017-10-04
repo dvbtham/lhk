d7f1c0b88050dc50228173fd726022ac6c183f2b
thamdavies <thamdv96@gmail.com>
2017-10-04 22:53:26 +0700

    add mobile header template.

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+	$(".bars").on('click', function(){
+		$('header ul').animate({ "width":'200px' }, 100);
+		$("header ul").show();
+		$(this).hide();
+	});
+	
+	$(".close-colapse").on('click', function(){
+		$('header ul').animate({ "width":'0px' }, 100);
+		$("header ul").hide();
+		$(".bars").show();
+	});
+});
