54fa6a992ffae10d7ee596523ab68e667ded9224
thamdavies <thamdv96@gmail.com>
2017-10-05 17:14:56 +0700

    resort the post category in home.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(function(){
 		$(this).hide();
 	});
 	
-	$(".close-colapse").on('click', function(){
+	$(".close-colapse, .container").on('click', function(){
 		$('header ul').animate({ "width":'0px' }, 100);
 		$("header ul").hide();
 		$(".bars").show();
