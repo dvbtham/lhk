14563fb43605e46adf44966c57c89bb1bbb3e780
thamdavies <thamdv96@gmail.com>
2017-10-06 13:10:35 +0700

    update template

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(function(){
 		$(this).hide();
 	});
 	
-	$(".close-colapse, .container").on('click', function(){
+	$(".close-colapse").on('click', function(){
 		$('header ul').animate({ "width":'0px' }, 100);
 		$("header ul").hide();
 		$(".bars").show();
